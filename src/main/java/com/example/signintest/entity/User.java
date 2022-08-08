f6636af87a9bed2e3e982a3bc09a2c90bd59039d
iujin <iuj09@naver.com>
2022-08-08 10:42:22 +0900

    User Entity password length 30 -> 300

--- src/main/java/com/example/signintest/entity/User.java
+++ src/main/java/com/example/signintest/entity/User.java
@@ -28,7 +28,7 @@ public class User extends BaseTime implements UserDetails{
     @Column(length = 100, nullable = false, unique = true)
     private String email;
 
-    @Column(length = 30, nullable = false)
+    @Column(length = 300, nullable = false)
     private String password;
 
     @Column
